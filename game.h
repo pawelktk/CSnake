33b09f202405a48424b98aedf85a18b8570d4417
pawelktk <pawel.klopotek@gmail.com>
2023-01-15 23:09:07 +0100

    Remove ignored files

--- game.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "main.h"
-extern int score;
-
-void game_init();
-void game_get_direction();
-void game_update();
--- main.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-int main() {
-  game_init();
-  while (1)
-    game_update();
-}
